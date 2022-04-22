b205cbd9e3d30ac1521e3a63f0783407d4e81eb9
DribDrob <drobushenko@gmail.com>
2022-04-22 22:25:09 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -18,6 +18,40 @@
 // Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета. .style.backgroundColor
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
 
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// const refs = {
+//   boxesEl: document.querySelector('#boxes'),
+//   amountEl: document.querySelector('input'),
+//   createEl: document.querySelector('[data-create]'),
+//   destroyEl: document.querySelector('[data-destroy]'),
+// }
+
+// refs.createEl.addEventListener("click", () => createBoxes(refs.amountEl.value))
+// refs.destroyEl.addEventListener("click", destroyBoxes)
+
+// function createBoxes(amount) {
+//  let boxElementsArr = [];
+ 
+//   for (let i=0; i<amount; i+=1) {
+//   const boxEl = document.createElement("div");
+//   boxEl.style.backgroundColor = getRandomHexColor();
+//   boxEl.style.width = '30px';
+//   boxEl.style.height = '30px';
+
+// if (i>0){
+//   boxEl.style.width =`${(20+i*10)+10}px`;
+//   boxEl.style.height = `${(20+i*10)+10}px`;}
+
+// boxElementsArr.push(boxEl);}
+
+// refs.boxesEl.append(...boxElementsArr);
+// }
+// function destroyBoxes() {
+//     refs.boxesEl.innerHTML='';
+// }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -31,21 +65,22 @@ const refs = {
 refs.createEl.addEventListener("click", () => createBoxes(refs.amountEl.value))
 refs.destroyEl.addEventListener("click", destroyBoxes)
 
+
 function createBoxes(amount) {
  let boxElementsArr = [];
- 
+
   for (let i=0; i<amount; i+=1) {
   const boxEl = document.createElement("div");
   boxEl.style.backgroundColor = getRandomHexColor();
   boxEl.style.width = '30px';
-  boxEl.style.height = '30px';
+  boxEl.style.height = '30px'; 
 
-if (i>0){
-  boxEl.style.width =`${(20+i*10)+10}px`;
-  boxEl.style.height = `${(20+i*10)+10}px`;}
-
-boxElementsArr.push(boxEl);}
-
+  if (refs.boxesEl.children.length !== 0){
+    boxEl.style.width =  `${Number.parseInt(refs.boxesEl.lastElementChild.style.width)+10}px`;
+    boxEl.style.height =  `${Number.parseInt(refs.boxesEl.lastElementChild.style.height)+10}px`;
+  }
+  boxElementsArr.push(boxEl);
+}
 refs.boxesEl.append(...boxElementsArr);
 }
 function destroyBoxes() {
